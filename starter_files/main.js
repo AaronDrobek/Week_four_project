2516b027dfdc5307f0bd24069ae62c549232317f
Aaron Drobek <adrobek09@gmail.com>
2017-08-12 11:47:30 -0400

    API data inserted, search function needs work

--- starter_files/main.js
+++ starter_files/main.js
@@ -7,3 +7,46 @@
 // 3. Create your `fetch` request that is called after a submission
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
+let search = document.getElementById("search");
+let results = document.querySelector(".results")
+let searchBar    = document.getElementById("searchBar");
+let button    = document.querySelector("button");
+
+button.addEventListener('click', function(){
+  // console.log("button was clicked");
+  let results = document.getElementById("results");
+
+  // let searchBar = searchBar.value;
+
+
+
+})
+
+let bar = searchBar.value;
+
+
+fetch("https://itunes.apple.com/search?term=" + bar )
+.then(
+  function(response) {
+    if (response.status !== 200) {
+      console.log(response.status);
+      return;
+    }
+    response.json().then(function(data) {
+      console.log("Here is the data:", data);
+
+
+
+      for (var i = 0; i < data.results.length; i++) {
+
+        let el1 = document.createElement("div");
+        el1.classList.add("el1");
+        el1.innerHTML=
+        `<img src=${data.results[i].artworkUrl100}>
+        <h4 class= "song_title">${data.results[i].trackName}</h4>
+        <h2 class= "artist_title" maxlength= "15">${data.results[i].artistName}</h2>
+        </div>`
+        results.appendChild(el1);
+      }
+    })
+  })
