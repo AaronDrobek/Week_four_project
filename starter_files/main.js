860a5451211d51d48802db89a3972bca5c34e8bf
Aaron Drobek <adrobek09@gmail.com>
2017-08-13 17:29:47 -0400

    brushed up some styling, search bar is working, audio tag needs work

--- starter_files/main.js
+++ starter_files/main.js
@@ -11,13 +11,22 @@ let search    = document.getElementById("search");
 let results   = document.querySelector(".results")
 let searchBar = document.getElementById("searchBar");
 let button    = document.querySelector("button");
+let musicPlayer = document.getElementsByClassName("musicPlayer");
+
+
+
+
 
 button.addEventListener('click', function(){
   // console.log("button was clicked");
   let searchBar = document.getElementById("searchBar");
 
   // let searchBar = searchBar.value;
-
+// searchBar.addEventListener('keypress', function (e) {
+//     var key = e.which || e.keyCode;
+//     if (key === 13) { // 13 is enter
+//       // code for enter
+//     }
 
 
     while(results.firstChild){
@@ -44,13 +53,58 @@ fetch("https://itunes.apple.com/search?term=" + bar )
 
         let el1 = document.createElement("div");
         el1.classList.add("el1");
+        // let preview = document.createElement("button");
+        // preview.setAttribute("src", "${data.results[i].previewUrl}")
+        // preview.addEventListener('click', function(){
+        //
+        // })
+
         el1.innerHTML=
         `<img src=${data.results[i].artworkUrl100}>
         <h4 class= "song_title">${data.results[i].trackName}</h4>
-        <h2 class= "artist_title" maxlength= "15">${data.results[i].artistName}</h2>
+
+        <h2 class= "artist_title">${data.results[i].artistName}</h2>
         </div>`
+        // let preview = document.createElement("input");
+        // preview.setAttribute("class", "preview");
+        // preview.textContent="preview";
+        //
+        // preview.setAttribute("value", `${data.results[i].previewUrl}`);
+        // let clip = preview.value;
+        // el1.appendChild(preview);
+        //
+        // preview.addEventListener('click', function(){
+        //   musicPlayer.load(clip);
+        //   console.log("clicked button");
+        // })
+
         results.appendChild(el1);
       }
+      // let play = function (){
+      // window.onload = function (){
+      //   musicPlayer.setAttribute('src', `${data.results[i].previewUrl}`);
+      //   }
+      // }
+
+
+
+        // musicPlayer.load()
+      })
+
     })
-  })
-})
+    })
+
+
+
+
+
+//
+//
+// let rsvp = document.createElement("button");
+//   let view = document.createElement("button");
+//   rsvp.setAttribute("class", "rsvp")
+//   view.setAttribute("class", "view")
+//   rsvp.textContent= "RSVP"
+//   view.textContent= "View Event"
+//   rsvp.addEventListener('click', function(){
+//     container.innerHTML = eventView;
